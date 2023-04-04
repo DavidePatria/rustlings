e551be9499b1ee92310e8ca841cf32c17fe6f18c
Davide <davide.patria@gmail.com>
2023-04-04 22:45:37 +0200

    many many done

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -16,11 +16,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +31,13 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    let lista : List = List::Nil;
+    lista
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let lista: List = List::Cons(1, Box::new(List::Nil));
+    lista
 }
 
 #[cfg(test)]
